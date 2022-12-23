e86ee82ec3bc62977e7039c6abe43429f3a806a4
fred84e0 <43129983+fred84e0@users.noreply.github.com>
2022-12-23 01:42:30 +0100

    - try api ci/cd

--- App/ICAdsApp/src/main.js
+++ App/ICAdsApp/src/main.js
@@ -46,6 +46,9 @@ router.beforeEach((to, from, next) => {
 
 // HTTP Request settings
 axios.defaults.baseURL = window.location.href.includes("127.0.0.1") ? 'https://localhost:7093/' : 'https://icads-api.azurewebsites.net';
+// axios.defaults.baseURL = 'https://icads-api.azurewebsites.net'
+// axios.defaults.baseURL = 'https://localhost:7093/'
+
 console.log(axios.defaults.baseURL)
 axios.interceptors.request.use(function (config) {
   if (!config.url.includes('login')) {
